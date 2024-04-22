30d8b21afd6106e43a581ee6246bf485dc21b73a
bulbasaus <114565392+bulbasaus@users.noreply.github.com>
2024-04-22 09:00:27 +0000

    uhh test

--- app/Actions/Jetstream/CreateTeam.php
+++ app/Actions/Jetstream/CreateTeam.php
@@ -16,6 +16,7 @@ class CreateTeam implements CreatesTeams
      * Validate and create a new team for the given user.
      *
      * @param  array<string, string>  $input
+     * @return Team
      */
     public function create(User $user, array $input): Team
     {
@@ -27,11 +28,9 @@ class CreateTeam implements CreatesTeams
 
         AddingTeam::dispatch($user);
 
-        $user->switchTeam($team = $user->ownedTeams()->create([
+        return $user->ownedTeams()->create([
             'name' => $input['name'],
             'personal_team' => false,
-        ]));
-
-        return $team;
+        ]);
     }
 }
